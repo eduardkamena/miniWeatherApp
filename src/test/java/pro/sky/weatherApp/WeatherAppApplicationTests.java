7e8d77cf00390114f86bdc04b2b9d3b2d2504619
Ed <eduard@kamenskikh.me>
2024-09-04 03:04:15 +0400

    Initial commit

---
+++ src/test/java/pro/sky/weatherApp/WeatherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.weatherApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
